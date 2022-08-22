43e828624576f7012d0df214b434f1d1fdce5f8a
MNekoKun <sanfilippo.gabriele05@gmail.com>
2022-08-22 23:13:00 +0200

    bug fixes

--- changeBackground/main.js
+++ changeBackground/main.js
@@ -1,5 +1,7 @@
 function changeColor(){
     let color = document.getElementsByTagName('input')[0].value
+    color=color.split(' ').join('')
+    console.log(color)
     document.body.style.backgroundColor=color;
     document.getElementsByTagName('input')[0].style.color=color;
 }
