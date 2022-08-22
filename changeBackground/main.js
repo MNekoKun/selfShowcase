9b988ddf62ac0d4dc84d568bdaa47ed02a306795
MNekoKun <sanfilippo.gabriele05@gmail.com>
2022-08-22 16:45:10 +0200

    fade

--- changeBackground/main.js
+++ changeBackground/main.js
@@ -1,14 +1,7 @@
 function changeColor(){
-    let colors = ['#712B75', '#C74B50', '#D49B54', '#346751', '#292929'];
-    let color = Math.floor(Math.random()*colors.length);
-    
-    while (color===last){
-        color = Math.floor(Math.random()*colors.length);
-    }
-
-    document.body.style.backgroundColor=colors[color];
-    last=color;
+    let color = document.getElementsByTagName('input')[0].value
+    document.body.style.backgroundColor=color;
+    document.getElementsByTagName('input')[0].style.color=color;
 }
 
-let last = '#292929';
 document.getElementsByTagName('button')[0].onclick=changeColor;
